ba1b0ce406dfbf70853a23d117c182f3f10bdbd5
you21979 <you2197901@gmail.com>
2016-05-18 20:27:18 +0900

    mod

--- libs/webapp.js
+++ libs/webapp.js
@@ -2,7 +2,12 @@
 const koa = require('koa');
 const koaBody = require('koa-body');
 const ip = require('request-ip');
+const moment = require('moment');
+const util = require('util');
+const Log4js = require('log4js')
+
 const start = exports.start = dirname => {
+    const logger = Log4js.getLogger('webserver');
     const app = koa();
 
     app
@@ -11,6 +16,20 @@ const start = exports.start = dirname => {
             this.client = {
                 remote_addr : ip.getClientIp(this.req),
             }
+            yield next;
+        })
+        .use(function* (next){
+            const DEFAULT = "%s %s -- %s %s HTTP/%s, %s %s";
+            const req = this.request, header = req.header, nodeReq = this.req;
+            const str = util.format(DEFAULT,
+                moment().format("YYYY/MM/DD HH:mm:ss"),
+                req.ip,
+                req.method,
+                req.url,
+                nodeReq.httpVersion,
+                req.length || null,
+                header['user-agent']);
+            logger.debug(str);
             yield next;
         })
         ;
