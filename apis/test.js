4785219a17f85fc15586891923a625552f58a048
you21979 <you2197901@gmail.com>
2016-05-18 21:09:10 +0900

    mod

--- apis/test.js
+++ apis/test.js
@@ -1,4 +1,5 @@
 const _ = require('koa-route');
+const rp = require('request-promise');
 const api = module.exports = base_path => {
     const makePath = path => base_path + '/test' + path
     return [
@@ -10,6 +11,9 @@ const api = module.exports = base_path => {
         }),
         _.get(makePath('/ctx'), (ctx) => {
             ctx.body = JSON.stringify(ctx.client, null, 2);
+        }),
+        _.get(makePath('/yahoo'), (ctx, name) => { // async
+            ctx.body = rp("http://www.yahoo.co.jp");
         }),
         _.get(makePath('/users/:name'), (ctx, name) => {
             ctx.body = 'Hello, ' + name + '!';
