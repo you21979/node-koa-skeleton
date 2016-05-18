c601297d312f9343802ed3fbe3eb60743d34b762
you21979 <you2197901@gmail.com>
2016-05-18 19:58:17 +0900

    mod

--- libs/webapp.js
+++ libs/webapp.js
@@ -1,15 +1,19 @@
 'use strict'; 
 const koa = require('koa');
+const jsonBody = require('koa-json-body');
 const ip = require('request-ip');
 const start = exports.start = dirname => {
     const app = koa();
 
-    app.use(function *(next){
-        this.client = {
-            remote_addr : ip.getClientIp(this.req),
-        }
-        yield next;
-    })
+    app
+        .use(jsonBody({ limit: '10kb' }))
+        .use(function *(next){
+            this.client = {
+                remote_addr : ip.getClientIp(this.req),
+            }
+            yield next;
+        })
+        ;
 
     const apis = require(dirname + "/apis");
     const base_path = '/api/v0';
